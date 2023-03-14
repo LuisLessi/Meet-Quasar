c30dc65868c27a85a0a7ab41ffdd110bdac26090
luisadsfatec@hotmail.com <luisadsfatec@hotmail.com>
2023-03-14 13:10:24 -0300

    Projeto finalizado

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar class="row justify-between">
+        <q-img src="../assets/logo-meet.png" width="200px" class="q-ma-md">
+        </q-img>
+      <div class="row">
+        <div class="q-mr-lg">
+          <q-icon name="help_outline" size="25px" 
+          class="q-my-sm q-mx-md" color="grey-7"></q-icon>
+          <q-icon name="sms_failed" size="25px" 
+          class="q-my-sm q-mx-md" color="grey-7"></q-icon>
+          <q-icon name="settings" size="25px" 
+          class="q-my-sm q-mx-md" color="grey-7"></q-icon>
+        </div>
+
+        <div>
+          <q-icon name="apps" size="25px" 
+          class="q-my-sm q-mx-md" color="grey-7">
+        </q-icon>
+        <q-avatar class="q-mr-sm">
+          <img src="https://cdn.quasar.dev/img/avatar.png">
+        </q-avatar>
+        </div>
+      </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default ({
+  name: 'MainLayout',
+  data() {
+    return {
+
+    }
+  }
+})
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,58 @@
+<template>
+  <q-page class="row justify-around full-width">
+    <div style="width: 50%;" class="q-pa-xl column justify-center">
+      <h3 class="q-ma-none">VideoChamadas premium.</h3>
+      <h3 class="q-mt-none q-mx-none">Agora gratuitas para todos.</h3>
+
+      <span style="font-size: 20px; color: #5f6368;">Reformulamos o Google Meet, nosso serviço seguro para reuniões de negócios. Agora ele é aberto e gratuito para todos</span>
+      
+      <div class="row" style="margin: 70px 0px">
+        <q-btn color="secondary" icon="video_call"
+        class="q-mr-lg" label="Nova reunião" />
+        <q-input filled v-model="text" :dense="dense" 
+        label="Digite um código ou link" style="width: 300px;">
+        <template v-slot:prepend>
+          <q-icon name="keyboard" />
+        </template>
+      </q-input>
+      </div>
+
+      <q-separator class="q-mb-xl"></q-separator>
+
+      <span style="font-size: 18px; color: #5f6368;">
+       <a href="" style="color: #056B5F; text-decoration: none;">Saiba mais</a> sobre o Google Meet
+      </span>
+    </div>
+    <div style="width: 50%;" class="row items-center justify-center">
+      <q-card>
+      <q-card-section class="my-card">
+        <q-img src="../assets/image-right.png" width="600px"></q-img>
+      </q-card-section>
+    </q-card>
+    </div>
+     
+  </q-page>
+</template>
+
+<script>
+
+export default ({
+  name: 'IndexPage',
+
+  data() {
+    return {
+      text: "",
+      dense: false,
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.my-card{
+  .q-img{
+    border-radius: 7px;
+  }
+}
+
+</style>
